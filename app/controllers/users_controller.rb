c4d8d59e312523098c335f33ba9820d51b0df99d
Crystal Cooling <cooling.cne@gmail.com>
2015-03-26 09:35:26 -0500

    reworked pwd match logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     
-    if @user.password == @user.password_confirmation
+    byebug
+    
+    if params["password"] == params["password_confirmation"]
       if @user.save
         session[:user_id] = @user.id
         redirect_to "/select_resorts" and return

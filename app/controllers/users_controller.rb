191fa07683ba183e816dcc489b2e76abcb58d52f
Crystal Cooling <cooling.cne@gmail.com>
2015-03-26 09:43:32 -0500

    removed byebug stmnt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     
-    byebug
-    
     if params["password"] == params["password_confirmation"]
       if @user.save
         session[:user_id] = @user.id
